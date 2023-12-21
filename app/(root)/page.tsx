c7e34f491df8e2c50a48ae161635b93f84696ccf
Mashrab Yo'ldoshev <mashrabyoldoshev@Mashrabs-MacBook-Air.local>
2023-12-21 18:10:26 +0500

    added new pages and payments logic with stripe

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,24 @@
+import CategoryFilter from "@/components/shared/category-filter";
+import Collection from "@/components/shared/collection";
+import Search from "@/components/shared/search";
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6,
+  });
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -22,7 +38,7 @@ export default function Home() {
 
           <Image
             src="/assets/images/hero.png"
-            alt="hero img"
+            alt="hero"
             width={1000}
             height={1000}
             className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
@@ -38,9 +54,20 @@ export default function Home() {
           Trust by <br /> Thousands of Events
         </h2>
 
-        <div className="flex flex-col gap-5 w-full md:flex-row">
-          
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
+          <CategoryFilter />
         </div>
+
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={page}
+          totalPages={events?.totalPages}
+        />
       </section>
     </>
   );
